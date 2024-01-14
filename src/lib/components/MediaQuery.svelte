ee2882c869e3df00608a4653788bb78276132ed5
furkankarayel <furkarayel@hotmail.com>
2024-01-14 22:34:39 +0100

    go

--- src/lib/components/MediaQuery.svelte
+++ src/lib/components/MediaQuery.svelte
@@ -1,9 +1,12 @@
 <script>
 	import { onMount } from 'svelte';
 
+	// @ts-ignore
 	export let query;
 
+	// @ts-ignore
 	let mql;
+	// @ts-ignore
 	let mqlListener;
 	let wasMounted = false;
 	let matches = false;
@@ -15,21 +18,26 @@
 		};
 	});
 
+	// @ts-ignore
 	$: {
 		if (wasMounted) {
 			removeActiveListener();
+			// @ts-ignore
 			addNewListener(query);
 		}
 	}
 
+	// @ts-ignore
 	function addNewListener(query) {
 		mql = window.matchMedia(query);
+		// @ts-ignore
 		mqlListener = (v) => (matches = v.matches);
 		mql.addListener(mqlListener);
 		matches = mql.matches;
 	}
 
 	function removeActiveListener() {
+		// @ts-ignore
 		if (mql && mqlListener) {
 			mql.removeListener(mqlListener);
 		}
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,3 +1,27 @@
+<script>
+	let status = '';
+	// @ts-ignore
+	const handleSubmit = async (data) => {
+		status = 'Submitting...';
+		const formData = new FormData(data.currentTarget);
+		const object = Object.fromEntries(formData);
+		const json = JSON.stringify(object);
+		const response = await fetch('https://api.web3forms.com/submit', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json'
+			},
+			body: json
+		});
+		const result = await response.json();
+		if (result.success) {
+			console.log(result);
+			status = 'Ihre E-Mail wurde erfolgreich versendet ';
+		}
+	};
+</script>
+
 <svelte:head>
 	<title>Kastonia.de Kontaktaufnahme</title>
 </svelte:head>
@@ -13,7 +37,21 @@
 			Wir sind hier, um Ihnen zuzuhören und Unterstützung zu bieten. Teilen Sie uns Ihre Wünsche und
 			Fragen über unser Kontaktformular mit
 		</p>
-		<form action="#" class="space-y-8">
+		<form on:submit|preventDefault={handleSubmit} class="space-y-8">
+			<input type="hidden" name="access_key" value="36766029-f272-4f5b-a7b9-664e011c5182" />
+			<input type="hidden" name="subject" value="Kontaktaufnahme durch das Formular" />
+			<div>
+				<label for="Name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+					>Ihr Name</label
+				>
+				<input
+					type="text"
+					id="name"
+					class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+					placeholder="Max Mustermann"
+					required
+				/>
+			</div>
 			<div>
 				<label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
 					>Ihre E-Mail</label
@@ -26,34 +64,30 @@
 					required
 				/>
 			</div>
-			<div>
-				<label for="subject" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-					>Thema</label
-				>
-				<input
-					type="text"
-					id="subject"
-					class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-					placeholder="Wie können wir Ihnen helfen?"
-					required
-				/>
-			</div>
+
 			<div class="sm:col-span-2">
 				<label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
 					>Ihre Nachricht</label
 				>
 				<textarea
 					id="message"
+					name="message"
 					rows="6"
 					class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
 					placeholder="Beschreiben Sie ihr Anliegen..."
 				/>
 			</div>
+
+			<!-- <div class="h-captcha" data-captcha="true" /> -->
 			<button
 				type="submit"
 				class="py-3 px-5 text-sm font-medium text-center shadow-lg text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
 				>Abschicken</button
 			>
+
+			<div>{status}</div>
 		</form>
+
+		<script src="https://web3forms.com/client/script.js" async defer></script>
 	</div>
 </section>
