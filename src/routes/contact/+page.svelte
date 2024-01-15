8feaa1f82b9569ac4c1b3b92911a0e8d2c9d5dba
furkankarayel <furkarayel@hotmail.com>
2024-01-15 23:06:31 +0100

    catpcha enabled

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -78,7 +78,7 @@
 				/>
 			</div>
 
-			<!-- <div class="h-captcha" data-captcha="true" /> -->
+			<div class="h-captcha" data-captcha="true" />
 			<button
 				type="submit"
 				class="py-3 px-5 text-sm font-medium text-center shadow-lg text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
