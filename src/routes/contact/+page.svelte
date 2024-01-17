4088740402944ffdba6da0d75b5799b45d352815
furkankarayel <furkarayel@hotmail.com>
2024-01-17 16:45:21 +0100

    contact form fix

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -52,10 +52,21 @@
 				>
 				<input
 					type="text"
-					id="name"
+					name="name"
 					class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
 					placeholder="Max Mustermann"
 					required
+				/>
+			</div>
+			<div>
+				<label for="Name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+					>Ihre Mobilfunknummer (optional)</label
+				>
+				<input
+					type="text"
+					name="mobile"
+					class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+					placeholder="Bsp.: +49 171 123456"
 				/>
 			</div>
 			<div>
@@ -64,7 +75,7 @@
 				>
 				<input
 					type="email"
-					id="email"
+					name="email"
 					class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
 					placeholder="max@mustermann.de"
 					required
