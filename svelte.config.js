a218f6dc38e50b5ef351434cc0b5e6eb0b7cfd3b
furkankarayel <furkarayel@hotmail.com>
2024-01-17 02:45:40 +0100

    asdasdasdasd

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
-import vercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import { resolve } from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,16 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: vercel(),
-		vite: {
-			resolve: {
-				alias: {
-					$src: resolve('./src'),
-					$components: resolve('./src/components'),
-					$sections: resolve('./src/sections')
-				}
-			}
-		}
+		adapter: adapter()
 	}
 };
 
