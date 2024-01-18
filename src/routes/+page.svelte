4f0b1f93b53ba3c5dadba06b7c2203c9db8c44a0
furkankarayel <furkarayel@hotmail.com>
2024-01-18 01:19:28 +0100

    done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -72,7 +72,7 @@
 			<div class="mx-auto px-6 md:px-12 xl:px-32">
 				<div class="text-center">
 					<div
-						class="mt-[-170px] block rounded-lg px-6 py-12 bg-white/30 backdrop-blur-xl md:py-16 md:px-12"
+						class="mt-[-170px] block rounded-lg px-6 py-12 bg-white/30 backdrop-blur-xl shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] md:py-16 md:px-12"
 					>
 						<h1
 							class="drop-shadow-lg mb-10 xs:text-3xl sm:text-3xl md:text-3xl xl:text-5xl lg:text-center font-bold tracking-tight"
