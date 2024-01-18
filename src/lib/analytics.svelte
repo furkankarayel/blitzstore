dc3dff92faa8a777165a45a20bd42353c25a12eb
furkankarayel <furkarayel@hotmail.com>
2024-01-18 01:36:16 +0100

    analytics test

---
+++ src/lib/analytics.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	$: {
+		if (typeof gtag !== 'undefined') {
+			gtag('config', 'G-R7YM0XWQKW', {
+				page_title: document.title,
+				page_path: $page.url.pathname
+			});
+		}
+	}
+</script>
+
+<svelte:head>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-R7YM0XWQKW">
+	</script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+
+		gtag('js', new Date());
+		gtag('config', 'G-R7YM0XWQKW');
+	</script>
+</svelte:head>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,14 @@
 	import Navbar from '$lib/components/Navbar.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import { PUBLIC_SHOP_NAME } from '$env/static/public';
+	import Analytics from '$lib/analytics.svelte';
 </script>
 
 <svelte:head>
 	<title>Home | {PUBLIC_SHOP_NAME}</title>
 </svelte:head>
 
+<Analytics />
 <Navbar />
 <slot />
 <Footer />
