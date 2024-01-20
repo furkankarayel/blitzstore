94b4cc86cdd8e3c304286634806c4ab8e946ea69
furkankarayel <furkarayel@hotmail.com>
2024-01-20 01:05:35 +0100

    pocketbase shop relation is working now

--- src/routes/products/[slug]/+page.server.ts
+++ src/routes/products/[slug]/+page.server.ts
@@ -1,16 +1,21 @@
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import { PUBLIC_SHOP_DB_NAME } from '$env/static/public';
 
 export const load = (async ({ params, locals }) => {
 	try {
+		const shop = await locals.pb
+			.collection(`shops`)
+			.getFirstListItem(`name = '${PUBLIC_SHOP_DB_NAME}'`);
 		const productRecord = await locals.pb
 			.collection('products')
-			.getFirstListItem(`slug="${params.slug}"`, {
+			.getFirstListItem(`slug="${params.slug}" && shop="${shop.id}"`, {
 				expand: 'related_products'
 			});
 
 		return structuredClone(productRecord);
 	} catch (e) {
+		console.log(e);
 		console.log(`Couldnt load page /shop/${params.slug}`);
 		throw error(500, {
 			message: 'Couldnt load this page'
--- src/routes/shop/[slug]/+page.server.ts
+++ src/routes/shop/[slug]/+page.server.ts
@@ -1,10 +1,15 @@
+import { PUBLIC_SHOP_DB_NAME } from '$env/static/public';
 import type { PageServerLoad } from './$types';
 import { error } from '@sveltejs/kit';
 
 export const load = (async ({ params, locals }) => {
 	try {
+		const shop = await locals.pb
+			.collection(`shops`)
+			.getFirstListItem(`name = '${PUBLIC_SHOP_DB_NAME}'`);
 		if (params.slug === 'all') {
 			const productsRecords = await locals.pb.collection('products').getList(1, 50, {
+				filter: `slug="${params.slug}" && shop="${shop.id}"`,
 				sort: '-created'
 			});
 
@@ -16,7 +21,7 @@ export const load = (async ({ params, locals }) => {
 			.getFirstListItem(`slug="${params.slug}"`);
 
 		const productsRecords = await locals.pb.collection('products').getList(1, 50, {
-			filter: `categories ~ "${category.id}"`,
+			filter: `categories ~ "${category.id}" && shop="${shop.id}"`,
 			sort: '-created'
 		});
 
