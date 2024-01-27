dceb5fefca48a411623c92903f633a5c9f776b40
furkankarayel <furkarayel@hotmail.com>
2024-01-27 15:32:24 +0100

    Production dynamic prerendering fully working

--- src/routes/products/[slug]/+page.server.ts
+++ src/routes/products/[slug]/+page.server.ts
@@ -1,28 +1,6 @@
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { PUBLIC_SHOP_DB_NAME } from '$env/static/public';
-
-export const load = (async ({ params, locals }) => {
-	try {
-		const shop = await locals.pb
-			.collection(`shops`)
-			.getFirstListItem(`name = '${PUBLIC_SHOP_DB_NAME}'`);
-		const productRecord = await locals.pb
-			.collection('products')
-			.getFirstListItem(`slug="${params.slug}" && shop="${shop.id}"`, {
-				expand: 'related_products'
-			});
-
-		return structuredClone(productRecord);
-	} catch (e) {
-		console.log(e);
-		console.log(`Couldnt load page /shop/${params.slug}`);
-		throw error(500, {
-			message: 'Couldnt load this page'
-		});
-	}
-}) satisfies PageServerLoad;
-
 import PocketBase from 'pocketbase';
 const pb = new PocketBase('https://app.kastonia.de');
 
@@ -38,5 +16,26 @@ myObjects.shift();
 export function entries() {
 	return myObjects;
 }
+
+export const load = (async ({ params, locals }) => {
+	try {
+		const shop = await locals.pb
+			.collection(`shops`)
+			.getFirstListItem(`name = '${PUBLIC_SHOP_DB_NAME}'`);
+		const productRecord = await locals.pb
+			.collection('products')
+			.getFirstListItem(`slug="${params.slug}" && shop="${shop.id}"`, {
+				expand: 'related_products'
+			});
+
+		return structuredClone(productRecord);
+	} catch (e) {
+		console.log(e);
+		console.log(`Couldnt load page /products/${params.slug}`);
+		throw error(500, {
+			message: 'Couldnt load this page'
+		});
+	}
+}) satisfies PageServerLoad;
 
 export const prerender = true;
