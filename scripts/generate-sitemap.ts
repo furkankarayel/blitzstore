326f53517a40d4b2c800679c449b4ed3218faa10
furkankarayel <furkarayel@hotmail.com>
2024-05-16 02:41:25 +0200

    cf sitemap works?

--- scripts/generate-sitemap.ts
+++ scripts/generate-sitemap.ts
@@ -78,8 +78,8 @@ getProductPages().then(function () {
 	// If you use the script in postbuild mode uses
 	// For vercel deployment use:
 	//fs.writeFileSync('.vercel/output/static/sitemap.xml', sitemap);
-	fs.writeFileSync('.svelte-kit/output/client/sitemap.xml', sitemap);
-	//fs.writeFileSync('static/sitemap.xml', sitemap);
+	//fs.writeFileSync('.svelte-kit/output/client/sitemap.xml', sitemap);
+	fs.writeFileSync('static/sitemap.xml', sitemap);
 });
 
 //If you want to put products and categories in the sitemap
