8ee8c08e2897947f32c4ea28e8dc0a46bfca5496
Furkan Karayel <furkan.karayel@outlook.de>
2024-05-28 22:52:48 +0200

    go

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,25 +18,3 @@
 <Navbar />
 <slot />
 <Footer />
-
-<style>
-	:root {
-		--font-body: Arial, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
-			Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-		--font-mono: 'Fira Mono', monospace;
-		--color-bg-0: rgb(75, 86, 87);
-		--color-bg-1: hsl(249, 36%, 86%);
-		--color-bg-2: hsl(255, 44%, 95%);
-		min-height: 100vh;
-		margin: 0;
-		background-attachment: fixed;
-		background-color: var(--color-bg-1);
-		background-size: 100vw 100vh;
-		background-image: radial-gradient(
-				50% 50% at 50% 50%,
-				rgba(255, 255, 255, 0.75) 0%,
-				rgba(255, 255, 255, 0) 100%
-			),
-			linear-gradient(180deg, var(--color-bg-0) 0%, var(--color-bg-1) 15%, var(--color-bg-2) 50%);
-	}
-</style>
