613bd1b41f953f9adb29c6a6acc2e1c9be6d4b5a
Furkan Karayel <furkan.karayel@outlook.de>
2024-05-29 00:23:15 +0200

    test rounded

--- src/lib/components/FeaturedProductFull.svelte
+++ src/lib/components/FeaturedProductFull.svelte
@@ -14,7 +14,7 @@
 >
 	<img class="h-64 w-full object-cover rounded-3xl object-center" src={image} alt={name} />
 	<div
-		class="bg-gray-900 bg-opacity-50 flex items-center absolute inset-0 bg-gradient-to-r from-black/60"
+		class="bg-gray-900 bg-opacity-50 rounded-3xl flex items-center absolute inset-0 bg-gradient-to-r from-black/60"
 	>
 		<div class="px-10 xs:px-3 max-w-xl">
 			<h2 class="text-2xl text-white font-semibold">{name}</h2>
