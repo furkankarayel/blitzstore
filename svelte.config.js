4d6bec05cc005bfbd22cfe54f6b0ac930481a51f
furkan karayel <furkan.karayel@outlook.de>
2024-08-27 21:00:40 +0200

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,23 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [vitePreprocess()],
+	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({})
+		adapter: adapter({
+			// default options are fine for most static deployments
+			// these can be customized as needed
+			strict: false,
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			routes: { include: ['/*'], exclude: ['<all>', '/sitemap.xml'] }
+		})
 	}
 };
 
