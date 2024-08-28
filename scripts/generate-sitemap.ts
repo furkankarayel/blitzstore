083546ea4905ec2cc53a548e32903d179e5b31ff
furkan karayel <furkan.karayel@outlook.de>
2024-08-28 10:56:28 +0200

    go

--- scripts/generate-sitemap.ts
+++ scripts/generate-sitemap.ts
@@ -85,8 +85,8 @@ getProductPages().then(function () {
 
 	// If you use the script in postbuild mode uses
 	// For vercel deployment use:
-	//fs.writeFileSync('.vercel/output/static/sitemap.xml', sitemap);
-	fs.writeFileSync('.svelte-kit/cloudflare/sitemap.xml', sitemap);
+	fs.writeFileSync('.vercel/output/static/sitemap.xml', sitemap);
+	//fs.writeFileSync('.svelte-kit/cloudflare/sitemap.xml', sitemap);
 	//fs.writeFileSync('../static/sitemap.xml', sitemap);
 });
 
--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,14 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess()],
 
 	kit: {
-		adapter: adapter({
-			// default options are fine for most static deployments
-			// these can be customized as needed
-			strict: false,
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false,
-			routes: { include: ['/*'], exclude: ['<all>', '/sitemap.xml'] }
-		})
+		adapter: adapter({})
 	}
 };
 
